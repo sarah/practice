13e46e59f7ee9de665a06ca861934492ea8a6401
Sarah Gray <sarah@Sarah-Grays-MacBook-Pro.local>
2010-05-04 08:04:02 -0500

    removing specific .out file

---
+++ C/f2c.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+/* print Fahrenheit-Celcius table
+   for fahr = 0, 20, ..., 300 */
+
+main()
+{
+	int fahr, celcius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step  = 20;
+
+	fahr = lower; 
+	while(fahr <= upper) {
+		celcius = 5 * (fahr-32) / 9;
+		printf("%d\t%d\n", fahr, celcius);
+		fahr = fahr + step;
+	}
+}
