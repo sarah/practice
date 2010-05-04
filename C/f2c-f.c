6179fc6aa9afa943320f553c291a8b65b972691f
Sarah Gray <sarah@Sarah-Grays-MacBook-Pro.local>
2010-05-04 08:15:56 -0500

    floating point c-f

--- C/f2c-f.c
+++ C/f2c-f.c
@@ -5,7 +5,7 @@
 
 main()
 {
-	int fahr, celcius;
+	float fahr, celcius;
 	int lower, upper, step;
 
 	lower = 0;
@@ -15,8 +15,8 @@ main()
 	fahr = lower; 
 	printf("FAHR\tCELC\n");
 	while(fahr <= upper) {
-		celcius = 5 * (fahr-32) / 9;
-		printf("%d\t%d\n", fahr, celcius);
+		celcius = (5.0/9.0) * (fahr-32) ;
+		printf("%3.0f %6.1f\n", fahr, celcius);
 		fahr = fahr + step;
 	}
 }
