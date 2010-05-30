0b0e707c76dd04c7bcf7e949a01edf7a6326671f
Sarah Gray <sarahg.gray@gmail.com>
2010-05-29 22:41:03 -0500

    extracted some constants

--- game_of_life/three.rb
+++ game_of_life/three.rb
@@ -1,29 +1,29 @@
-require 'timeout'
+ # 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+ # 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
+ # 3. Any live cell with two or three live neighbours lives on to the next generation.
+ # 4. Any dead cell with exactly three live neighbours becomes a live cell.
 class Cell
+  DeathNumber = 3
+  DeathThreshold = 2
+  LifeNumber = 3
+  
   def initialize
     @alive = 0.1 > rand
-    # puts "in initialize: @alive: #{@alive}; #{@alive.class}"
   end
   def alive?
-    # puts "@alive: #{@alive}; #{@alive.class}"
     @alive
   end
   def to_s
    alive? ? "o" : " "
   end
-  # used for summing active_neighbors count
   def to_i
     alive? ? 1 : 0
   end
-   # 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-   # 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
-   # 3. Any live cell with two or three live neighbours lives on to the next generation.
-   # 4. Any dead cell with exactly three live neighbours becomes a live cell.
   def next_state!(alive_neighbor_count)
     if alive?
-      @alive = false if (alive_neighbor_count < 2) || (alive_neighbor_count >3)
+      @alive = false if (alive_neighbor_count < DeathThreshold) || (alive_neighbor_count >DeathNumber)
     else
-      @alive = true if alive_neighbor_count == 3
+      @alive = true if alive_neighbor_count == LifeNumber
     end
   end
 end
@@ -50,13 +50,13 @@ class Game
   def play
     draw
     @steps.times do 
-       get_state
+       next_state
        `clear`
        draw
      end
   end
   # calculate whether each cell should be alive or dead and return new grid
-  def get_state
+  def next_state
     @grid.each_with_index do |row, y|
       row.each_with_index do |cell, x|
         cell = @grid[y][x]
@@ -67,19 +67,14 @@ class Game
           [y+1,x-1], [y+1,x], [y+1,x+1]  # bottom
         ]
         neighbors = get_neighbors(possible_neighbors, cell_coords)
-        # p "coords #{cell_coords.inspect}: neighbors: #{neighbors.size}: #{neighbors.inspect}"
         num_active_neighbors = count_active_neighbors(neighbors)
-        # puts "I am currently  #{cell.alive? ? "alive" : "dead" } and I have #{num_active_neighbors} active neighbors."
         cell.next_state!(num_active_neighbors)
-        # puts "I am now: #{cell.alive? ? "alive" : "dead"}"
-        # puts "-------"
       end
     end
   end
   def get_neighbors(possible_neighbors,cell_coords)
     possible_neighbors.select do |neighbor_coords|
       y,x = neighbor_coords[0], neighbor_coords[1]
-      # puts "y: #{y}; x: #{x}; width: #{@width}; height: #{@height}"
       !neighbor_coords.include?(-1) && (x < @width) && (y < @height)
     end
   end
