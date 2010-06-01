4470ac714779000d26e420c9761a2f01d7523067
Sarah Gray <sarahg.gray@gmail.com>
2010-05-31 20:37:04 -0500

    renamed files

--- 7-languages/Ruby/day_three/exercise.rb
+++ 7-languages/Ruby/day_three/exercise.rb
@@ -1,58 +1,47 @@
 class CsvRow
-  attr_accessor :row, :header
-  def initialize(header, row_content)
-    @header = header
-    @row = row_content
+  def initialize(headers, items)
+    @headers,@items = headers, items
   end
   def method_missing name, *args
-    puts row if name.to_s.downcase == header.to_s.downcase
+    index = @headers.index name.to_s
+    index ? @items[index] : "There is no column titled #{name}"
   end
 end
 module ActsAsCsv
   def self.included(base)
     base.extend ClassMethods
   end
-
   module ClassMethods
     def acts_as_csv
       include InstanceMethods
     end
   end
-
   module InstanceMethods
-    def read
-      @csv_contents = []
-      filename = self.class.to_s.downcase + '.txt'
-      file = File.new(filename)
-      @headers = file.gets.chomp.split(', ')
-
-      @csv_contents = file.map do |row|
-        row.chomp.split(', ')
-      end
-    end
-
     attr_accessor :headers, :csv_contents
-
-    def each(&block)
-      @headers.each_with_index do |header, index|
-        row_content = @csv_contents.map do |arr|
-          arr[index]
-        end
-        yield CsvRow.new(header, row_content)
-      end
-    end
-
     def initialize
       read
+    end
+    def each
+      csv_contents.each {|row| yield CsvRow.new(@headers, row)}
+    end
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + ".txt"
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(", ")
+
+      file.each do |row|
+        @csv_contents << row.chomp.split(", ")
+      end
     end
   end
 end
 
-
-class RubyCsv 
+class RubyCsv
   include ActsAsCsv
   acts_as_csv
 end
 
+
 csv = RubyCsv.new
-csv.each{|row| puts row.dogs}
+csv.each{|row| puts row.one}
---
+++ 7-languages/Ruby/day_three/misunderstood-exercise.rb
@@ -0,0 +1,58 @@
+class CsvRow
+  attr_accessor :row, :header
+  def initialize(header, row_content)
+    @header = header
+    @row = row_content
+  end
+  def method_missing name, *args
+    puts row if name.to_s.downcase == header.to_s.downcase
+  end
+end
+module ActsAsCsv
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    def acts_as_csv
+      include InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    def read
+      @csv_contents = []
+      filename = self.class.to_s.downcase + '.txt'
+      file = File.new(filename)
+      @headers = file.gets.chomp.split(', ')
+
+      @csv_contents = file.map do |row|
+        row.chomp.split(', ')
+      end
+    end
+
+    attr_accessor :headers, :csv_contents
+
+    def each(&block)
+      @headers.each_with_index do |header, index|
+        row_content = @csv_contents.map do |arr|
+          arr[index]
+        end
+        yield CsvRow.new(header, row_content)
+      end
+    end
+
+    def initialize
+      read
+    end
+  end
+end
+
+
+class RubyCsv 
+  include ActsAsCsv
+  acts_as_csv
+end
+
+csv = RubyCsv.new
+csv.each{|row| puts row.one}
